da1c26a39576a9a67c4ef4bd8a9c21353e185a2d
Matt Mechtley <mmechtley@gmail.com>
2016-01-27 01:18:29 -0500

    whoops, strip_drizzle_keys still used pyfits. Fixed that, as well as a python 2.x print statement

--- scripts/strip_drizzle_keys.py
+++ scripts/strip_drizzle_keys.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
+"""
+astrodrizzle keeps certain fits header keywords from every one of the original
+files used to create the final, drizzled file. For large or deep survey fields,
+this may be header keywords from hundreds of individual images, and can
+significantly contribute to the file size of small stamp images extracted from
+the full, combined image. This utility removes these DXXX keywords from an
+image's fits header.
+
+Usage:
+strip_drizzle_keys.py fits1.fits [...] fitsn.fits
+"""
 import sys
-import pyfits
+from astropy.io import fits
 import re
 
 # Single-drizzle keywords begin with D followed by 3 digits
 _key_pat = re.compile('^D\d{3}')
 
 def strip_drizzle_keys(filename):
-    with pyfits.open(filename, mode='update') as fd:
+    with fits.open(filename, mode='update') as fd:
         for key, value in fd[0].header.items():
             if _key_pat.match(key):
                 del fd[0].header[key]
         fd.flush()
-    print 'Removed single-driz keywords from {}'.format(filename)
+    print('Removed single-driz keywords from {}'.format(filename))
     return
 
 if __name__ == '__main__':
